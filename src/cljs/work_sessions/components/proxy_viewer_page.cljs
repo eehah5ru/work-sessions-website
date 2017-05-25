ea77883358829487e92f3c3aa1367648643f9b12
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-05-25 21:17:04 +0300

    headers view -> headers, basic proxy-viewer-page

--- src/cljs/work_sessions/components/proxy_viewer_page.cljs
+++ src/cljs/work_sessions/components/proxy_viewer_page.cljs
@@ -3,7 +3,11 @@
    [re-frame.core :refer [dispatch subscribe]]
    [reagent.core :as r]
    [goog.string :as gstring :refer [unescapeEntities]]
+
+   [work-sessions.components.headers :refer [headers-view]]
+   [work-sessions.utils :as u]
+
    ))
 
 (defn proxy-viewer-page []
-  [:h1 "aaa"])
+  [headers-view])
