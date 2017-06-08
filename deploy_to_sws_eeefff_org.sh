c2341523814b57826070282fc2b2dd10b87b4ac7
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2017-06-08 13:32:40 +0300

    added post deploy showing docs in proxy room, regenerating dev files

--- deploy_to_sws_eeefff_org.sh
+++ deploy_to_sws_eeefff_org.sh
@@ -6,4 +6,6 @@ echo "deploying to sws.eeefff.org" &&
     lein scss :dev once &&
     LEIN_SNAPSHOTS_IN_RELEASE=true lein with-profile production uberjar &&
     scp ./target/uberjar/work-sessions.jar work-sessions.do.myfutures.trade:/var/sws-eeefff-org &&
-    ssh work-sessions.do.myfutures.trade "sudo service sws-eeefff-org restart; sleep 10; sudo service sws-eeefff-org status"
+    ssh work-sessions.do.myfutures.trade "sudo service sws-eeefff-org restart; sleep 10; sudo service sws-eeefff-org status" &&
+    curl http://sws.eeefff.org/proxy-viewer/show-docs &&
+    lein with-profile dev filegen-ng
